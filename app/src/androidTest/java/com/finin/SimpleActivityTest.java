28bf81c4276948a08387b5f9fa38435ddbd9b14a
Shubham Goel <shubham.goel@91springboard.com>
2020-07-26 20:46:59 +0530

    [add] testcase to check if there is data on screen

--- app/src/androidTest/java/com/finin/SimpleActivityTest.java
+++ app/src/androidTest/java/com/finin/SimpleActivityTest.java
@@ -1,4 +1,34 @@
 package com.finin;
 
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.finin.views.activities.UserListActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
 public class SimpleActivityTest {
+
+    @Rule
+    public ActivityTestRule<UserListActivity> activityRule =
+            new ActivityTestRule<>(UserListActivity.class);
+
+    @Test
+    public void userDataAvailable() {
+        Espresso.onView(withText("George Bluth")).check(matches(isDisplayed()));
+    }
+
+
 }
+
